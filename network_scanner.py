28a652cb6456bf70a5665adcc220a8c0eb916fc1
sivaramr <sivaram.rasathurai@syscolabs.com>
2019-10-28 01:55:40 +0530

    -Improve the BNS

--- network_scanner.py
+++ network_scanner.py
@@ -1,60 +1,60 @@
-import scapy.all as scapy
+#!bin/bash sudo python
 import optparse
 
+import scapy.all as scapy
 from scapy.layers.inet import IP, ICMP
 
-LINUX_TTL = 64
 
-
-def get_arguments():
+def getArguments():
     parser = optparse.OptionParser()
     parser.add_option("-t", "--target", dest="target", help="target IP/ IP range")
     args_options, arguments = parser.parse_args()
     return args_options
 
 
-def create_packet(ip):
+def createPacket(ip):
     arp_request = scapy.ARP(pdst=ip)  # create a ARP request object by scapy
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # we have set the destination
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # We have set the destination
     arp_request_broadcast = broadcast / arp_request
-    return arp_request_broadcast
+    return (arp_request_broadcast)
 
 
-def transmit_packet(packet):
+def transmitPacket(packet):
     success_list, failure_list = scapy.srp(packet, timeout=1)
     return success_list
 
 
-def get_os(ip_addr):
+def getOS(ip_addr):
+    ttl_values = {32: "Windows", 60: "MAC OS", 64: "Linux", 128: "Windows", 255: "Linux 2.4 Kernal"}
     ans = scapy.sr1(IP(dst=str(ip_addr)) / ICMP(), timeout=1, verbose=0)
-    if not ans:
-        return "Not responding, probably Using Firewall"
-    if ans.ttl <= LINUX_TTL:
-        return "Linux/Unix"
+    if ans:
+        if ans.ttl in ttl_values:
+            return ttl_values.get(ans.ttl)
+        else:
+            return "could not figure the OS version"
     else:
-        return "Windows"
+        return "Packets could not send successfully"
 
 
-def parse_response(success_list):
-    return [
-        {
-            "ip": success[1].psrc,
-            "mac": success[1].hwsrc,
-        } for success in success_list
-    ]
+def parseResponse(success_list):
+    targets = []
+    for success in success_list:
+        entry = {'ip': success[1].psrc, 'mac': success[1].hwsrc}
+        targets.append(entry)
+    return targets
 
 
 def print_analysis(element_entries):
     print("IP\t\t\tMAC Address\t\t\tOPERATING SYSTEM")
-    print("."*100)
+    print("." * 100)
     for element in entries:
-        print(element["ip"] + "\t\t" + element['mac'] + "\t\t" + get_os(element["ip"]) + "\n")
+        print(element["ip"] + "\t\t" + element['mac'] + "\t\t" + getOS(element["ip"]) + "\n")
 
 
-options = get_arguments()
+options = getArguments()
 
-if options.target:
-    broadcast_packets = create_packet(options.target)
-    success_packets = transmit_packet(broadcast_packets)
-    entries = parse_response(success_packets)
+if options.target is not None:
+    broadcast_packets = createPacket(options.target)
+    success_packets = transmitPacket(broadcast_packets)
+    entries = parseResponse(success_packets)
     print_analysis(entries)
